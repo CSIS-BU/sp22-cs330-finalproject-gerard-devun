e26719c62a75c06e6e42f26ca985d06e5f032df6
Devun41 <68511661+Devun41@users.noreply.github.com>
2022-04-16 15:02:39 -0500

    Update client
    added the player guessing the word and checking if letters are in the correct place and if they are in the word as a whole, put place holder comments for checking if the letter is in the word but in the wrong spot, as well as a place where it adds the letter to a string that holds letters that are not in the word to guess. Made fixes to opening the file to make sure the integer holding the number of letters wanted in the word to guess is converted to a string before loading which number of letter txt file.

--- client-c.c
+++ client-c.c
@@ -20,6 +20,22 @@ main(int argc, char * argv[])
 
     //holds number of letters in guessing word
     int numLetters;
+    //boolean for if the word is guessed correctly
+    bool correctWord=false;
+    //word to guess
+    char wordToGuess[];
+    //guessed word
+    char userWord[];
+    //number of attempted guesses
+    int guessAttempts=0;
+    //letters guessed correctly, size of the wordToGuess
+    char correctLetters[];
+    //list of letters not in word
+    char incorrectLetters[];
+    //list of letters guessed that are in word
+    char lettersInWord[];
+    //letters in word but wrong spot, size of the wordToGuess
+    char wrongSpotLetters[];
 
     if (argc==3) { 
         host = argv[1]; 
@@ -55,11 +71,61 @@ main(int argc, char * argv[])
         scanf("%*s");
     }
     //opens txt file based on number of letters wanted, can read or write file
-    *fp=fopen(numLetters+"L.txt","r+");
+    *fp=fopen(""+ numLetters +"L.txt","r+");
+    //if file isnt found, print an error and exit the program
     if(!fp){
         printf("ERROR: Could not find "+ numLetters + "letter database file.");
+        exit(1);
     }
+
+    /* sets correctLetters and wrongSpotLetters string to length of word and fill with astrics, will replace astrics with letters guessed correctly below */
+    for(int i=0;i<=numLetters;i++){
+        correctLetters[i]="*";
+        wrongSpotLetters[i]="*";
+    }
+
+    /* sets the word to guess to the first string found in file*/
+    /*DELETE COMMENT BEFORE FINAL TURN IN **DOUBLE CHECK IF +1 IS NECESSARY** fgets reads a line from the specified stream and stores it into the string pointed to by wordToGuess. 
+    It stops when either (n-1) (numLetters+1 -1) characters are read, the newline character is read, or the end-of-file is reached, 
+    whichever comes first. via TutorialsPoint */
+    fgets(*wordToGuess,numLetters+1,*fp);
     
+    /* player guessing the word */
+    while(correctWord==false && guessAttempts!=5){
+        printf("Enter your guess: ");
+        scanf("%s",userWord); 
+        if(userWord.length()!=numLetters){
+            printf("Incorrect word length entered.");
+        }
+        //if want to see if its a valid word: found database txt file https://raw.githubusercontent.com/dwyl/english-words/master/words.txt
+        /* checking if letters are in word */
+        else if(){
+            for(int i=0;i<=numLetters;i++){
+                //check if letters are in the correct place, if they are: add to correctLetters[] and lettersInWord[]
+                if(userWord[i]==wordToGuess[i]){
+                    correctLetters[i]=userWord[i];
+                    strncat(lettersInWord,&userWord[i],1);
+                    strncat(lettersInWord,", ",1);
+                }
+                //check if letters are in word, if they are add to lettersInWord[], checks if letter is already in list first
+                if(strchr(*lettersInWord,userWord[i])==NULL){
+                    if(strchr(*wordToGuess,userWord[i])){
+                        strncat(lettersInWord,&userWord[i],1);
+                        strncat(lettersInWord,", ",1);
+                    }
+                }
+               
+                /** might need seperate for loops for each of these **/
+
+                //check if letters are in lettersInWord but NOT in correctLetters[], if so add to wrongSpotLetters[]
+                if(userWord[i])
+                //
+                //if letters are not in word add to incorrectLetters[]
+            }
+        }   
+
+    }
+
     int size; 
     while( (size=fread(buf, 1, MAX_SIZE, stdin) )) {  
         if(send(s, buf, size, 0) < 0) { 
